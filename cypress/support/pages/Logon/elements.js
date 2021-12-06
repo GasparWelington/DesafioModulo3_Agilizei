c3ab15d0936c30807cb26cf45f67530ac52be49a
welington <wgt_tere@hotmail.com>
2021-12-05 22:01:06 -0300

    seconnt commit

--- cypress/support/pages/Logon/elements.js
+++ cypress/support/pages/Logon/elements.js
@@ -1,54 +1,36 @@
-var Chance = require('chance');
+var Chance = require("chance");
 var chance = new Chance();
 
 import { groupCollapsed } from "console";
 
-
 export const ELEMENTS = {
+  Cadastro: {
+    ConfirmarSeEstouNaPaginaCadastroCliente: "h3:contains(Create an account)",
+    Email: "#email_create",
+    CriarConta: "#SubmitCreate",
+    // ConfirmarPaginaDePrencherDadosAbriu:':nth-child(1) > .page-subheading'
+    ConfirmarPaginaDePrencherDadosAbriu:
+      "h3:contains(Your personal information)",
 
-    Cadastro:{
-
-        ConfirmarSeEstouNaPaginaCadastroCliente :'h3:contains(Create an account)',
-        Email:'#email_create',
-        CriarConta:'#SubmitCreate',
-        ConfirmarPaginaDePrencherDadosAbriu:':nth-child(1) > .page-subheading',
-        MarcarCheckMrs:'input[value=2]',
-        Nome:'#customer_firstname',
-        SobreNome:'#customer_lastname',
-        Senha:'#passwd',
-        Dia:'#days',
-        Mes:'#months',
-        Ano:'#years',
-        Empresa:'#company',
-        Endereco:'#address1',
-        EndrecoAlternativo:'#address2',
-        Cidade:'#city',
-        UF:'#id_state',
-        Pais:'#id_country',
-        InformamacoesAdicionais:'#other',
-        NumeroResidencial:'#phone',
-        Cel:'#phone_mobile',
-        EmailAlternativo:'#alias',
-        ConfirmarRegistro:'#submitAccount',
-        Cep:'#postcode',
-        ValidarContaCadastrada:'.info-account'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
- 
-    }
-}
+    MarcarCheckMrs: "input[value=2]",
+    Nome: "#customer_firstname",
+    SobreNome: "#customer_lastname",
+    Senha: "#passwd",
+    Dia: "#days",
+    Mes: "#months",
+    Ano: "#years",
+    Empresa: "#company",
+    Endereco: "#address1",
+    EndrecoAlternativo: "#address2",
+    Cidade: "#city",
+    UF: "#id_state",
+    Pais: "#id_country",
+    InformamacoesAdicionais: "#other",
+    NumeroResidencial: "#phone",
+    Cel: "#phone_mobile",
+    EmailAlternativo: "#alias",
+    ConfirmarRegistro: "#submitAccount",
+    Cep: "#postcode",
+    ValidarContaCadastrada: ".info-account",
+  },
+};
